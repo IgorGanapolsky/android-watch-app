00e63e44c91f54292a0560bab5eb8e590fc621d0
Igor Ganapolsky <iganapolsky@gmail.com>
2019-03-14 10:27:08 -0400

    SmokeTests

--- app/src/androidTest/java/com/igorganapolsky/vibratingwatchapp/SmokeTests.kt
+++ app/src/androidTest/java/com/igorganapolsky/vibratingwatchapp/SmokeTests.kt
@@ -1,12 +1,8 @@
 package com.igorganapolsky.vibratingwatchapp
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.core.app.ActivityScenario
-import androidx.test.runner.AndroidJUnit4
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
-
 
 @RunWith(AndroidJUnit4::class)
 class SmokeTests {
